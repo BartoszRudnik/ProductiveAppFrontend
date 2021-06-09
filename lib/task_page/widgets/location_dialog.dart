fd9f692849d0620e21228ad4cc3625b811065cdf
Bartosz Rudnik <rudnik49@gmail.com>
2021-06-09 23:37:11 +0200

    naprawa bledu wyboru lokalizacji poprzez wpisanie adresu

--- lib/task_page/widgets/location_dialog.dart
+++ lib/task_page/widgets/location_dialog.dart
@@ -64,131 +64,129 @@ class LocationDialogState extends State<LocationDialog> with TickerProviderState
     return StatefulBuilder(
       builder: (context, setState) {
         return AlertDialog(
-          content: Stack(
-            children: [
-              Container(
-                height: 600,
-                width: 400,
-                child: FlutterMap(
-                  mapController: _mapController,
-                  options: MapOptions(
-                    onTap: (point) {
-                      setState(() {
-                        this.widget.choosenLocation.latitude = point.latitude;
-                        this.widget.choosenLocation.longitude = point.longitude;
-                      });
-                    },
-                    center: LatLng(widget.choosenLocation.latitude, widget.choosenLocation.longitude),
-                    zoom: 10.0,
-                  ),
-                  layers: [
-                    TileLayerOptions(
-                      urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                      subdomains: ['a', 'b', 'c'],
+          content: Container(
+            width: double.maxFinite,
+            child: Stack(
+              children: [
+                Container(
+                  height: 600,
+                  width: 400,
+                  child: FlutterMap(
+                    mapController: _mapController,
+                    options: MapOptions(
+                      onTap: (point) {
+                        setState(() {
+                          this.widget.choosenLocation.latitude = point.latitude;
+                          this.widget.choosenLocation.longitude = point.longitude;
+                        });
+                      },
+                      center: LatLng(widget.choosenLocation.latitude, widget.choosenLocation.longitude),
+                      zoom: 10.0,
                     ),
-                    MarkerLayerOptions(
-                      markers: [
-                        Marker(
-                          width: 150.0,
-                          height: 150.0,
-                          point: LatLng(widget.choosenLocation.latitude, widget.choosenLocation.longitude),
-                          builder: (context) => Icon(
-                            Icons.location_on,
-                            color: Colors.red,
-                          ),
-                        )
-                      ],
-                    )
-                  ],
+                    layers: [
+                      TileLayerOptions(
+                        urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                        subdomains: ['a', 'b', 'c'],
+                      ),
+                      MarkerLayerOptions(
+                        markers: [
+                          Marker(
+                            width: 150.0,
+                            height: 150.0,
+                            point: LatLng(widget.choosenLocation.latitude, widget.choosenLocation.longitude),
+                            builder: (context) => Icon(
+                              Icons.location_on,
+                              color: Colors.red,
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-                child: Card(
-                  child: Container(
-                    height: 150,
-                    child: Column(
-                      children: [
-                        TextField(
-                          controller: _textEditingController,
-                          decoration: InputDecoration(
-                            prefixIcon: Icon(Icons.location_on),
-                            hintText: "Search for location",
-                            contentPadding: EdgeInsets.all(16.0),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                  child: Card(
+                    child: Container(
+                      height: this.searchString.length > 2 ? 180 : 52,
+                      child: Column(
+                        children: [
+                          TextField(
+                            controller: _textEditingController,
+                            decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.location_on),
+                              hintText: "Search for location",
+                              contentPadding: EdgeInsets.all(16.0),
+                            ),
+                            onChanged: (search) async {
+                              await getPlacemarks(context, search);
+                              setState(() {
+                                searchString = search;
+                              });
+                            },
                           ),
-                          onChanged: (search) async {
-                            await getPlacemarks(context, search);
-                            setState(() {
-                              searchString = search;
-                            });
-                          },
-                        ),
-                        if (searchString != null && searchString.length >= 3 && placemarks != [])
-                          Container(
-                            height: 180,
-                            child: SingleChildScrollView(
-                              child: Container(
-                                child: ListView.builder(
-                                  shrinkWrap: true,
-                                  itemCount: this.placemarks.length,
-                                  itemBuilder: (context, index) => ListTile(
-                                    title: Text(placemarks[index].key.locality + ", " + placemarks[index].key.street),
-                                    subtitle: Text(placemarks[index].key.country),
-                                    onTap: () {
-                                      setState(
-                                        () {
-                                          LatLng point = LatLng(placemarks[index].value.latitude, placemarks[index].value.longitude);
-                                          widget.choosenLocation.latitude = point.latitude;
-                                          widget.choosenLocation.longitude = point.longitude;
-                                          _animatedMapMove(point, 15.0);
-                                          print(widget.choosenLocation.latitude.toString() + "," + widget.choosenLocation.longitude.toString());
-                                          _textEditingController.clear();
-                                          searchString = '';
-                                        },
-                                      );
-                                    },
-                                  ),
+                          if (searchString != null && searchString.length >= 3 && placemarks != [])
+                            Flexible(
+                              child: ListView.builder(
+                                shrinkWrap: true,
+                                itemCount: this.placemarks.length,
+                                itemBuilder: (context, index) => ListTile(
+                                  title: Text(placemarks[index].key.locality + ", " + placemarks[index].key.street),
+                                  subtitle: Text(placemarks[index].key.country),
+                                  onTap: () {
+                                    setState(
+                                      () {
+                                        LatLng point = LatLng(placemarks[index].value.latitude, placemarks[index].value.longitude);
+                                        widget.choosenLocation.latitude = point.latitude;
+                                        widget.choosenLocation.longitude = point.longitude;
+                                        _animatedMapMove(point, 15.0);
+                                        print(widget.choosenLocation.latitude.toString() + "," + widget.choosenLocation.longitude.toString());
+                                        _textEditingController.clear();
+                                        searchString = '';
+                                      },
+                                    );
+                                  },
                                 ),
                               ),
                             ),
-                          ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-                      child: Column(
-                        children: [
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              primary: Theme.of(context).primaryColor,
-                              side: BorderSide(color: Theme.of(context).primaryColor),
-                            ),
-                            onPressed: () {
-                              Navigator.of(context).pop(widget.choosenLocation);
-                            },
-                            child: Text(
-                              'Add location',
-                              style: TextStyle(
-                                fontSize: 14,
-                                color: Theme.of(context).accentColor,
-                              ),
-                            ),
-                          ),
                         ],
                       ),
                     ),
-                  )
-                ],
-              )
-            ],
+                  ),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                        child: Column(
+                          children: [
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                primary: Theme.of(context).primaryColor,
+                                side: BorderSide(color: Theme.of(context).primaryColor),
+                              ),
+                              onPressed: () {
+                                Navigator.of(context).pop(widget.choosenLocation);
+                              },
+                              child: Text(
+                                'Add location',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: Theme.of(context).accentColor,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
           ),
         );
       },
