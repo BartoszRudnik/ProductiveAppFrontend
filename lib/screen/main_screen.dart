704ba0bdf94d4992091c40e505ede7a42c40185d
BartoszRudnik <rudnik49@gmail.com>
2021-08-30 17:20:35 +0200

    sprawdzania polaczenia z internetem

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -10,6 +10,7 @@ import '../widget/drawer/main_drawer.dart';
 import 'loading_task_screen.dart';
 import 'tabs_screen.dart';
 import 'task_details_screen.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 
 class MainScreen extends StatefulWidget {
   static const routeName = '/main-screen';
@@ -20,19 +21,39 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   void onClickedNotification(String payload) async {
-    Task task = Provider.of<TaskProvider>(context, listen: false).taskList.firstWhere((element) => element.id == int.parse(payload), orElse: () => null);
+    Task task = Provider.of<TaskProvider>(context, listen: false)
+        .taskList
+        .firstWhere((element) => element.id == int.parse(payload),
+            orElse: () => null);
 
     if (task == null) {
-      await Provider.of<LocationProvider>(context, listen: false).getLocations();
-      await Provider.of<TaskProvider>(context, listen: false).fetchSingleTaskFull(int.parse(payload));
+      await Provider.of<LocationProvider>(context, listen: false)
+          .getLocations();
+      await Provider.of<TaskProvider>(context, listen: false)
+          .fetchSingleTaskFull(int.parse(payload));
 
-      task = Provider.of<TaskProvider>(context, listen: false).taskList.firstWhere((element) => element.id == int.parse(payload));
+      task = Provider.of<TaskProvider>(context, listen: false)
+          .taskList
+          .firstWhere((element) => element.id == int.parse(payload));
     }
 
-    Navigator.of(context).pushNamed(TaskDetailScreen.routeName, arguments: task);
+    Navigator.of(context)
+        .pushNamed(TaskDetailScreen.routeName, arguments: task);
   }
 
-  void listenNotifications() => Notifications.onNotifications.stream.listen(onClickedNotification);
+  void listenNotifications() =>
+      Notifications.onNotifications.stream.listen(onClickedNotification);
+
+  void listenInternetChanges() =>
+      Connectivity().onConnectivityChanged.listen((connectionResult) {
+        if (checkInternetConnection(connectionResult)) {
+          Data.loadData(context);
+        }
+      });
+
+  bool checkInternetConnection(ConnectivityResult result) {
+    return result != ConnectivityResult.none;
+  }
 
   Future<void> future;
 
@@ -43,7 +64,9 @@ class _MainScreenState extends State<MainScreen> {
 
     Notifications.initLocalization();
     Notifications.initNotification();
+
     listenNotifications();
+    listenInternetChanges();
 
     super.initState();
   }
@@ -52,16 +75,17 @@ class _MainScreenState extends State<MainScreen> {
   Widget build(BuildContext context) {
     return FutureBuilder(
       future: this.future,
-      builder: (ctx, loadResult) => loadResult.connectionState == ConnectionState.waiting
-          ? LoadingTaskScreen()
-          : Scaffold(
-              body: Stack(
-                children: [
-                  MainDrawer(),
-                  TabsScreen(),
-                ],
-              ),
-            ),
+      builder: (ctx, loadResult) =>
+          loadResult.connectionState == ConnectionState.waiting
+              ? LoadingTaskScreen()
+              : Scaffold(
+                  body: Stack(
+                    children: [
+                      MainDrawer(),
+                      TabsScreen(),
+                    ],
+                  ),
+                ),
     );
   }
 }
