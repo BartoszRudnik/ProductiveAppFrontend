19f1f4ca9e67be82a6babff9c0314806c6e41b37
BartoszRudnik <rudnik49@gmail.com>
2021-10-05 23:08:44 +0200

    wyswietlanie wersji aplikacji

--- lib/screen/settings_screen.dart
+++ lib/screen/settings_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:productive_app/provider/task_provider.dart';
 import 'package:productive_app/widget/settings_language.dart';
+import 'package:productive_app/widget/settings_version.dart';
 import 'package:provider/provider.dart';
 
 import '../provider/auth_provider.dart';
@@ -318,6 +319,8 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 SettingsGraphicSettings(),
                 SizedBox(height: 10),
                 SettingsLanguage(),
+                SizedBox(height: 10),
+                SettingsVersion(),
               ],
             ),
           ),
