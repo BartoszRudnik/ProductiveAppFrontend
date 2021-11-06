c44b534ff1a3e1389ce58be3ad14222e3d5950a8
BartoszRudnik <rudnik49@gmail.com>
2021-11-07 00:18:25 +0100

    dodanie do okna wyboru lokalizacji podgladu wybranego punktu na mapie

--- lib/widget/single_location.dart
+++ lib/widget/single_location.dart
@@ -20,75 +20,77 @@ class SingleLocation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Dismissible(
-      key: ValueKey(location),
-      background: Container(
-        padding: EdgeInsets.symmetric(horizontal: 10),
-        alignment: Alignment.centerLeft,
-        color: Theme.of(context).primaryColor,
-        child: Row(
-          children: [
-            Icon(
-              Icons.edit,
-              color: Theme.of(context).accentColor,
-              size: 50,
+    return location.localizationName == null
+        ? Container()
+        : Dismissible(
+            key: ValueKey(location),
+            background: Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              alignment: Alignment.centerLeft,
+              color: Theme.of(context).primaryColor,
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.edit,
+                    color: Theme.of(context).accentColor,
+                    size: 50,
+                  ),
+                  Text(
+                    AppLocalizations.of(context).edit,
+                    style: TextStyle(color: Theme.of(context).accentColor, fontSize: 20, fontWeight: FontWeight.w400),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              AppLocalizations.of(context).edit,
-              style: TextStyle(color: Theme.of(context).accentColor, fontSize: 20, fontWeight: FontWeight.w400),
+            secondaryBackground: Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              alignment: Alignment.centerRight,
+              color: Theme.of(context).primaryColor,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    AppLocalizations.of(context).delete,
+                    style: TextStyle(color: Theme.of(context).accentColor, fontSize: 20, fontWeight: FontWeight.w400),
+                  ),
+                  Icon(
+                    Icons.delete_outline,
+                    color: Theme.of(context).accentColor,
+                    size: 40,
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
-      ),
-      secondaryBackground: Container(
-        padding: EdgeInsets.symmetric(horizontal: 10),
-        alignment: Alignment.centerRight,
-        color: Theme.of(context).primaryColor,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Text(
-              AppLocalizations.of(context).delete,
-              style: TextStyle(color: Theme.of(context).accentColor, fontSize: 20, fontWeight: FontWeight.w400),
-            ),
-            Icon(
-              Icons.delete_outline,
-              color: Theme.of(context).accentColor,
-              size: 40,
-            ),
-          ],
-        ),
-      ),
-      confirmDismiss: (direction) async {
-        if (direction == DismissDirection.endToStart) {
-          bool hasAgreed = await Dialogs.showChoiceDialog(context, AppLocalizations.of(context).areSureDeleteLocation);
-          if (hasAgreed) {
-            Provider.of<TaskProvider>(context, listen: false).clearLocationFromTasks(location.uuid);
-            Provider.of<SynchronizeProvider>(context, listen: false).addLocationToDelete(location.uuid);
-            await Provider.of<SettingsProvider>(context, listen: false).deleteFilterLocation(location.uuid);
-            Provider.of<LocationProvider>(context, listen: false).deleteLocation(location.uuid);
-          }
-        }
-        if (direction == DismissDirection.startToEnd) {
-          this.editLocationForm(context, location);
-        }
-        return false;
-      },
-      child: Card(
-        child: ListTile(
-          title: Text(
-            location.localizationName,
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
-          ),
-          subtitle: location.locality.length <= 1 && location.street.length <= 1
-              ? Text(
-                  location.latitude.toStringAsFixed(3) + ", " + location.longitude.toStringAsFixed(3),
-                )
-              : Text(
-                  location.locality + ", " + location.street + ", " + location.country,
+            confirmDismiss: (direction) async {
+              if (direction == DismissDirection.endToStart) {
+                bool hasAgreed = await Dialogs.showChoiceDialog(context, AppLocalizations.of(context).areSureDeleteLocation);
+                if (hasAgreed) {
+                  Provider.of<TaskProvider>(context, listen: false).clearLocationFromTasks(location.uuid);
+                  Provider.of<SynchronizeProvider>(context, listen: false).addLocationToDelete(location.uuid);
+                  await Provider.of<SettingsProvider>(context, listen: false).deleteFilterLocation(location.uuid);
+                  Provider.of<LocationProvider>(context, listen: false).deleteLocation(location.uuid);
+                }
+              }
+              if (direction == DismissDirection.startToEnd) {
+                this.editLocationForm(context, location);
+              }
+              return false;
+            },
+            child: Card(
+              child: ListTile(
+                title: Text(
+                  location.localizationName,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
                 ),
-        ),
-      ),
-    );
+                subtitle: location.locality.length <= 1 && location.street.length <= 1
+                    ? Text(
+                        location.latitude.toStringAsFixed(3) + ", " + location.longitude.toStringAsFixed(3),
+                      )
+                    : Text(
+                        location.locality + ", " + location.street + ", " + location.country,
+                      ),
+              ),
+            ),
+          );
   }
 }
